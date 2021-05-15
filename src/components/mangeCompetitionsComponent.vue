2cee074c6a7a94eae9ced86f4cf3b9a98f4c63da
zeroexa <bezdnastada@gmail.com>
2021-05-15 03:03:26 +0300

    :sparkles: initial commit

---
+++ src/components/mangeCompetitionsComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-page class="q-pa-md relative">
+    <div class="q-pa-md flex column items-center justify-between">
+      <div class="buttonContainer">
+        <q-btn round outline color="positive" icon="add" />
+      </div>
+      <q-virtual-scroll :items="heavyList" class="virtualScroll">
+        <template v-slot="{ item, index }">
+          <q-item v-ripple="{ color: 'black' }" :key="index">
+            <q-item-section>
+              <q-item-label> #{{ index }} - {{ item.label }} </q-item-label>
+            </q-item-section>
+            <q-item-section avatar>
+              <q-fab
+                color="primary"
+                icon="keyboard_arrow_left"
+                direction="left"
+                flat
+              >
+                <q-fab-action color="negative" icon="delete_outline" flat />
+                <q-fab-action color="primary" icon="mode_edit_outline" flat />
+              </q-fab>
+            </q-item-section>
+          </q-item>
+        </template>
+      </q-virtual-scroll>
+    </div>
+  </q-page>
+</template>
+
+<script>
+const maxSize = 100;
+const heavyList = [];
+
+for (let i = 0; i < maxSize; i++) {
+  heavyList.push({
+    label:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit " + (i + 1),
+  });
+}
+
+Object.freeze(heavyList);
+
+export default {
+  name: "mangeCompetitionsComponent",
+  data() {
+    return {
+      heavyList,
+    };
+  },
+  methods: {},
+  created() {
+    console.log("i am created");
+  },
+};
+</script>
+
+<style scoped>
+.virtualScroll {
+  max-height: 80vh;
+  width: 70vw;
+}
+.buttonContainer {
+  width: 100%;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated flat class="q-pa-md bg-dark text-white flex flex-center">
+      <q-tabs
+        v-model="tab"
+        narrow-indicator
+        dense
+        align="center"
+        class="text-whie"
+      >
+        <q-tab
+          :ripple="{ color: 'white' }"
+          name="manageNews"
+          icon="unsubscribe"
+          label="Новости"
+        />
+        <q-tab
+          :ripple="{ color: 'white' }"
+          name="manageCompetitions"
+          icon="emoji_events"
+          label="Соревнования"
+        />
+        <q-tab
+          :ripple="{ color: 'white' }"
+          name="manageUsers"
+          icon="person"
+          label="Пользователи"
+        />
+      </q-tabs>
+    </q-header>
+    <q-page-container class="pContainer">
+      <q-tab-panels
+        v-model="tab"
+        animated
+        swipeable
+        transition-prev="fade"
+        transition-next="fade"
+        class="flex flex-center"
+      >
+        <q-tab-panel class="q-pa-none bg-grey-1 h-full" name="manageNews">
+          <manageNewsComponent />
+        </q-tab-panel>
+        <q-tab-panel class="q-pa-none bg-grey-1" name="manageCompetitions">
+          <mangeCompetitionsComponent />
+        </q-tab-panel>
+        <q-tab-panel class="q-pa-none bg-grey-1" name="manageUsers">
+          <manageUsersComponent />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import manageNewsComponent from "../components/manageNewsComponent";
+import mangeCompetitionsComponent from "../components/mangeCompetitionsComponent";
+import manageUsersComponent from "../components/manageUsersComponent";
+
+export default {
+  name: "PageIndex",
+  components: { manageNewsComponent, mangeCompetitionsComponent, manageUsersComponent },
+  data() {
+    return {
+      tab: "manageNews",
+    };
+  },
+};
+</script>
